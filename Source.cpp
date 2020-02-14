554e641709e4b1507052e70849e6fd105f6cd256
kimsof <60942753+kimsof@users.noreply.github.com>
2020-02-14 14:27:26 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int main() {
-	void function_sofa() {
-		std::cout << "func";
-	}
 	std::cout << "Goodbye";
 	int a = 53;
 	std::cout << a % 5;
 }
+void function_sofa() {
+		std::cout << "func";
+	}
