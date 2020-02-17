38c3342906762061578378eda97bd83fc149e8c3
Sofia Kim <sofiakim@phystech.edu>
2020-02-17 17:39:10 +0300

    Вывела свое имя.

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,8 @@
 int main() {
 	std::cout << "Goodbye";
 	int a = 53;
-	std::cout << 3 - 2 - 5;
+	std::cout << 3 * 2 * 5;
+	std::cout << "Ким София Сергеевна";
 }
 void function_sofa() {
 		std::cout << "func";
